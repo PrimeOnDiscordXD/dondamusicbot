9403e8136ca081e3e892f705d9e8e6ba083af846
Prime <47949562+Sodagame@users.noreply.github.com>
2022-05-25 15:21:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "token": "OTIzNjEzNzc1NDY1MTU2NjU4.GtVuKH.SDfClWWhLj13vM8M0rf9BW8iFUKqRnjOE3wsc8", // example token
+    "token": "OTIzNjEzNzc1NDY1MTU2NjU4.GtVuKH.SDfClWWhLj13vM8M0rf9BW8iFUKqRnjOE3wsc8", // example token 
 } 
